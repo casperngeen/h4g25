68f8af32c9b2c23315ad8a9b5aa39ff761f7d9bd
ningmeng618 <guboyuan@gmail.com>
2025-01-11 15:04:00 +0800

    Added login api

--- flask webserver/db.py
+++ flask webserver/db.py
@@ -22,7 +22,8 @@ def close_db(e=None):
 def init_db():
     db = get_db()
 
-    with current_app.open_resource("schema.sql") as f:
+    #Create Users table
+    with current_app.open_resource("./sql_scripts/users.sql") as f:
         db.executescript(f.read().decode("utf8"))
 
 @click.command("init-db")
