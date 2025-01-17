611a3be9dbf9c9df898477f9cf6c3314a3812b70
Casper Ng <casperngeen@gmail.com>
2025-01-17 12:38:20 +0800

    Bug fixes

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -1,4 +1,4 @@
-"use client";  
+"use client";
 
 import React, { useState } from "react";
 import { AccountCircle, ShoppingCart } from "@mui/icons-material";
@@ -12,80 +12,75 @@ const handleLogout = () => {};
 const handleCheckout = () => {};
 
 export const NavBar = () => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-  const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
 
-  return (
-    <Box
-      display="flex"
-      justifyContent="space-between"
-      padding="16px 8px"
-      borderBottom="1px solid"
-      borderColor={grey[200]}
-    >
-      <Image
-        src="/images/logo.png"
-        alt="logo"
-        width="250"
-        height="80"
-      />
-      <Box
-        display="flex"
-        flexDirection="row"
-        justifyContent="flex-end"
-        gap="8px"
-        alignItems="center"
-      >
-        <IconButton
-          sx={{
-            padding: 0,
-            borderRadius: "6px",
-            borderStyle: "hidden",
-            "&:hover": {
-              outline: "2px solid",
-              outlineColor: grey[600],
-            },
-          }}
-          onClick={handleCheckout}
+    return (
+        <Box
+            display="flex"
+            justifyContent="space-between"
+            padding="16px 8px"
+            borderBottom="1px solid"
+            borderColor={grey[200]}
         >
-          <ShoppingCart sx={{ color: "black", fontSize: "32px" }} />
-        </IconButton>
-        <IconButton
-          sx={{
-            padding: 0,
-            borderRadius: "6px",
-            borderStyle: "hidden",
-            "&:hover": {
-              outline: "2px solid",
-              outlineColor: grey[600],
-            },
-          }}
-          onClick={handleMenuClick}
-        >
-          <AccountCircle sx={{ fontSize: "32px" }} />
-        </IconButton>
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleMenuClose}
-        >
-          <MuiMenuItem onClick={handleClickProfile}>Profile</MuiMenuItem>
-          <MuiMenuItem onClick={handleLogout}>Log out</MuiMenuItem>
-        </Menu>
-      </Box>
-    </Box>
-  );
+            <Image src="/images/logo.png" alt="logo" width="250" height="80" />
+            <Box
+                display="flex"
+                flexDirection="row"
+                justifyContent="flex-end"
+                gap="8px"
+                alignItems="center"
+            >
+                <IconButton
+                    sx={{
+                        padding: 0,
+                        borderRadius: "6px",
+                        borderStyle: "hidden",
+                        "&:hover": {
+                            outline: "2px solid",
+                            outlineColor: grey[600],
+                        },
+                    }}
+                    onClick={handleCheckout}
+                >
+                    <ShoppingCart sx={{ color: "black", fontSize: "32px" }} />
+                </IconButton>
+                <IconButton
+                    sx={{
+                        padding: 0,
+                        borderRadius: "6px",
+                        borderStyle: "hidden",
+                        "&:hover": {
+                            outline: "2px solid",
+                            outlineColor: grey[600],
+                        },
+                    }}
+                    onClick={handleMenuClick}
+                >
+                    <AccountCircle sx={{ fontSize: "32px" }} />
+                </IconButton>
+                <Menu
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={handleMenuClose}
+                >
+                    <MenuItem onClick={handleClickProfile}>Profile</MenuItem>
+                    <MenuItem onClick={handleLogout}>Log out</MenuItem>
+                </Menu>
+            </Box>
+        </Box>
+    );
 };
 
 const MenuItem = styled(MuiMenuItem)(
-  ({ theme }) => `
+    ({ theme }) => `
   list-style: none;
   padding: 8px;
   border-radius: 8px;
